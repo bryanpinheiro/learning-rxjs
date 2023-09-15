de2253b5079ae5c44b3eea403d8f3e18b7fa0339
Bryan Pinheiro <bryansouza_01@outlook.com>
2023-09-15 02:12:59 -0300

    add rxjs basic concepts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import React, { useEffect } from 'react';
+import { useObservable, users } from '@/rxjs/mySubscription.service';
+
 export default function Home() {
+
+  useEffect(() => {
+    const mySubscription = useObservable((subscriber) => {
+      subscriber.next(users);
+    });
+
+    return () => {
+      mySubscription.unsubscribe();
+    };
+  }, []);
+
   return (
     <div>
-
+      <h1>Learning RxJS Basics</h1>
     </div>
   )
 }
---
+++ src/rxjs/mySubscription.service.ts
@@ -0,0 +1,127 @@
+/*
+ * This code demonstrates the use of RxJS operators to process data.
+ * It creates an observable, applies operators, and subscribes to it.
+ */
+
+import {
+  Observable,
+  Observer,
+  Subscriber,
+  Subscription,
+  TeardownLogic,
+} from "rxjs";
+import { map } from "rxjs/operators";
+
+/*
+ * Ingredients
+ */
+export const users = {
+  data: [
+    {
+      status: "active",
+      age: 14,
+    },
+    {
+      status: "inactive",
+      age: 32,
+    },
+    {
+      status: "inactive",
+      age: 53,
+    },
+    {
+      status: "active",
+      age: 17,
+    },
+    {
+      status: "inactive",
+      age: 11,
+    },
+    {
+      status: "inactive",
+      age: 32,
+    },
+    {
+      status: "inactive",
+      age: 43,
+    },
+    {
+      status: "active",
+      age: 23,
+    },
+  ],
+};
+
+export const users2 = {
+  data: [
+    {
+      status: "active",
+      age: 14,
+    },
+    {
+      status: "active",
+      age: 17,
+    },
+  ]
+}
+
+export const useObservable = (
+  subscriber:
+    | ((this: Observable<any>, subscriber: Subscriber<any>) => TeardownLogic)
+    | undefined
+) => {
+  /*
+   * Receives the ingredients from the subscriber
+   */
+
+  const observable: Observable<any> = new Observable(subscriber);
+
+  /*
+   * Operators operating ingredients inside the pipe
+   */
+
+  observable.pipe(
+    map((value) => {
+      console.log("1) Got data from observable");
+      return value.data;
+    }),
+    map((value) => {
+      console.log("2) Got data from first operator");
+      return value.filter((user: any) => user.status === "active");
+    }),
+    map((value) => {
+      console.log("3) Got data from second operator");
+      return (
+        value.reduce((sum: number, user: any) => sum + user.age, 0)
+      ) / value.length;
+    }),
+    map((value) => {
+      console.log("4) Got data from third operator", value);
+      if (value < 18) throw new Error("Average is too young");
+      else return value;
+    })
+  );
+
+  /*
+   * Display the final product
+   */
+  const observer: Observer<any> = {
+    next: (value: any) => {
+      console.log("Observer got a value of " + value);
+    },
+    error: (err: any) => {
+      console.log("Observer got an error of " + err);
+    },
+    complete: () => {
+      console.log("Observer got a complete notification");
+    },
+  };
+
+  /*
+   * Subscribe the observable
+   */
+
+  const subscription = observable.subscribe(observer);
+
+  return subscription;
+};
